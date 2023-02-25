6c448ec1028de236b56a925584bd35acf760f422
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2023-02-24 16:12:37 -0800

    finished chai tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -24,10 +24,10 @@ suite('Functional Tests', function () {
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=<your_name>')
+        .get('/hello?name=aronRoy')
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.text, 'hello <your_name>');
+          assert.equal(res.text, 'hello aronRoy');
           done();
         });
     });
@@ -36,26 +36,42 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({surname: "Colombo"})
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json')
+          assert.equal(res.body.name, 'Cristoforo')
+          assert.equal(res.body.surname, 'Colombo')
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+            chai
+        .request(server)
+        .put('/travellers')
+        .send({surname: "da Verrazzano"})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json')
+          assert.equal(res.body.name, 'Giovanni')
+          assert.equal(res.body.surname, 'da Verrazzano')
+          done();
+        });
     });
   });
 });
 
 const Browser = require('zombie');
+Browser.site = 'localhost:3000';
 
 suite('Functional Tests with Zombie.js', function () {
+  
   this.timeout(5000);
+const browser = new Browser();
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  })
 
 
 
@@ -68,14 +84,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success()
+          browser.assert.text('span#name', 'Cristoforo')
+          browser.assert.text('span#surname', 'Colombo')
+          browser.assert.elements('span#dates', 1)
+        })
+      })
 
       done();
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success()
+          browser.assert.text('span#name', 'Amerigo')
+          browser.assert.text('span#surname', 'Vespucci')
+          browser.assert.elements('span#dates', 1)
+        })
+      })
       done();
     });
   });
